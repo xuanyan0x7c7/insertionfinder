c06f7f475d27b55f72f166200bb6cf5be46d29d3
Xuan Yan <xuanyan@xuanyan.ws>
2017-07-13 13:21:59 +0800

    fix typo

--- src/utils/memory.h
+++ src/utils/memory.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <stdlib.h>
 
-#define MALLOC_HELPER(T, n, ...) (T*)malloc((n) * sizeof(T))
+#define MALLOC_HELPER(T, n, ...) (T*)SafeMalloc((n) * sizeof(T))
 #define MALLOC(...) MALLOC_HELPER(__VA_ARGS__, 1, 1)
 #define REALLOC(p, T, n) p = (T*)SafeRealloc(p, (n) * sizeof(T))
 
