56dff5943b081e076b7acb34c83a524621e0c5b8
Xuan Yan <xuanyan@xuanyan.ws>
2017-10-23 15:30:24 +0800

    public getters for cases

--- include/case.hpp
+++ include/case.hpp
@@ -17,33 +17,52 @@ namespace InsertionFinder {
 
     class Case {
     private:
-        Cube state;
-        std::vector<Algorithm> algorithm_list;
+        Cube _state;
+        std::vector<Algorithm> list;
     private:
-        std::uint32_t mask;
-        bool has_parity;
-        int corner_cycles;
-        int edge_cycles;
+        std::uint32_t _mask;
+        bool _has_parity;
+        int _corner_cycles;
+        int _edge_cycles;
     public:
         Case(const Cube& state);
     public:
         void save_to(std::ostream& out) const;
         void read_from(std::istream& in);
+    public:
+        const Cube& state() const {
+            return this->_state;
+        }
+        std::uint32_t mask() const {
+            return this->_mask;
+        }
+        bool has_parity() const {
+            return this->_has_parity;
+        }
+        int corner_cycles() const {
+            return this->_corner_cycles;
+        }
+        int edge_cycles() const {
+            return this->_edge_cycles;
+        }
+        const std::vector<Algorithm>& algorithm_list() const {
+            return this->list;
+        }
     private:
         bool contains_algorithm(const Algorithm& algorithm) const {
             return std::find(
-                this->algorithm_list.cbegin(), this->algorithm_list.cend(),
+                this->list.cbegin(), this->list.cend(),
                 algorithm
-            ) != this->algorithm_list.cend();
+            ) != this->list.cend();
         }
     public:
         void add_algorithm(const Algorithm& algorithm) {
             if (!this->contains_algorithm(algorithm)) {
-                this->algorithm_list.push_back(algorithm);
+                this->list.push_back(algorithm);
             }
         }
         void sort_algorithms() {
-            std::sort(this->algorithm_list.begin(), this->algorithm_list.end());
+            std::sort(this->list.begin(), this->list.end());
         }
     };
 };
